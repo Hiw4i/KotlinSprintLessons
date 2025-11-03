2a4220d3a7ef27066701571bf7fecab3e4f5a091
Hiw4i <slava.vaneev8@gmail.com>
2025-11-03 14:38:11 +0300

    KS-3-4 chess move

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var numberMove = 1
+
+    println("from $from to $to, number move: $numberMove")
+
+    from = "D2"
+    to = "D3"
+    numberMove += 1
+
+    println("from $from to $to, number move: $numberMove")
+}
