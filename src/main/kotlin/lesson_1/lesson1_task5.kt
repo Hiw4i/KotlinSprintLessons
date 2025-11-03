0e26f75a72681d7d128e2c468181e9829caea364
Hiw4i <slava.vaneev8@gmail.com>
2025-11-03 13:02:38 +0300

    KS-1-5 added constants secondsInHour and secondsInMinute, String.format()

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -3,14 +3,13 @@ package org.example.lesson_1
 fun main() {
     val totalSeconds: Short = 5409
 
-    val hours: Int = totalSeconds / 3600
-    val minutes: Int = (totalSeconds % 3600) / 60   //остаток от часов перевожу в минуты
-    val seconds: Int = totalSeconds % 60   //остаток от минут = оставшиеся сукунды
+    val secondsInHour = 3600
+    val secondsInMinute = 60
 
-    if (hours < 10) print("0$hours") else print(hours)
-    print(":")
-    if (minutes < 10) print("0$minutes") else print(minutes)
-    print(":")
-    if (seconds < 10) print("0$seconds") else print(seconds)
+    val hours: Int = totalSeconds / secondsInHour
+    val minutes: Int = (totalSeconds % secondsInHour) / secondsInMinute
+    val seconds: Int = totalSeconds % secondsInMinute
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 
 }
