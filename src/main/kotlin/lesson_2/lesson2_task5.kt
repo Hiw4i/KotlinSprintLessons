2d9cd9f31997429fa3d925fb21335077b856ac5f
Hiw4i <slava.vaneev8@gmail.com>
2025-11-03 13:24:48 +0300

    KS-2-5 added constants percentDivider and one

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,11 +1,16 @@
 package org.example.lesson_2
 
+import kotlin.math.pow
+
 fun main() {
     val summa: Double =  70000.0
     val annualRate: Double = 16.7
     val years: Double = 20.0
 
-    val finalSumma: Double = summa * Math.pow(1 + annualRate/100, years)
+    val percentDivider = 100
+    val one = 1
+
+    val finalSumma: Double = summa * (one + annualRate / percentDivider).pow(years)
 
     println(String.format("%.3f", finalSumma))
 }
