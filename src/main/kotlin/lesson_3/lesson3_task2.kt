4835ed264632898270d3927cad4f3111c0929f87
Hiw4i <slava.vaneev8@gmail.com>
2025-11-03 14:24:33 +0300

    KS-3-2 personal data change

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var lastName = "Андреева"
+    var age = 20
+
+    println("$firstName $middleName $lastName, $age лет")
+
+    age = 22
+    lastName = "Сидорова"
+
+    println("$firstName $middleName $lastName, $age года")
+}
