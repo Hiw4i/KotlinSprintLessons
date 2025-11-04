3e867ac7215121f217f79f64b1c0f2082fb77c1b
Hiw4i <slava.vaneev8@gmail.com>
2025-11-04 13:26:55 +0300

    KS-1-5 constants were written in capital letters

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_1
 
-const val secondsInHour = 3600
-const val secondsInMinute = 60
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
 
 fun main() {
     val totalSeconds: Short = 5409
 
-    val hours: Int = totalSeconds / secondsInHour
-    val minutes: Int = (totalSeconds % secondsInHour) / secondsInMinute
-    val seconds: Int = totalSeconds % secondsInMinute
+    val hours: Int = totalSeconds / SECONDS_IN_HOUR
+    val minutes: Int = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds: Int = totalSeconds % SECONDS_IN_MINUTE
 
     println("%02d:%02d:%02d".format(hours, minutes, seconds))
 
