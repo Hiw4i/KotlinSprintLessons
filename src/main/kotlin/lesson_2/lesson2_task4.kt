8202929ccc8902bfe39ff0c5b18bccadb0cd9501
Hiw4i <slava.vaneev8@gmail.com>
2025-11-04 13:35:22 +0300

    KS-2-4 deleted the Byte type

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_2
 
 fun main() {
-    val crystalOre: Byte = 7
-    val ironOre: Byte = 11
+    val crystalOre = 7
+    val ironOre = 11
 
-    val buff: Byte = 20
+    val buff = 20
 
     val percentDivider = 100
 
