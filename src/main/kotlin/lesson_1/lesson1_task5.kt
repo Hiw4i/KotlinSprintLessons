8fde358c0e9dabf45fa79df1fe706b104d0e33a7
Hiw4i <slava.vaneev8@gmail.com>
2025-11-04 13:23:38 +0300

    KS-1-5 added const variables

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_1
 
+const val secondsInHour = 3600
+const val secondsInMinute = 60
+
 fun main() {
     val totalSeconds: Short = 5409
-
-    val secondsInHour = 3600
-    val secondsInMinute = 60
 
     val hours: Int = totalSeconds / secondsInHour
     val minutes: Int = (totalSeconds % secondsInHour) / secondsInMinute
     val seconds: Int = totalSeconds % secondsInMinute
 
-    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 
 }
