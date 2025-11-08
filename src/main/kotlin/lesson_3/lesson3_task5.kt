60f640b6f0410a78198ce31584942d99d36f4929
Hiw4i <slava.vaneev8@gmail.com>
2025-11-08 16:11:05 +0300

    KS-3-5 used split("-", ";") instead of two split()

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,12 +3,11 @@ package org.example.lesson_3
 fun main() {
     val moveInfo = "D2-D4;0"
 
-    val splitMoveInfo = moveInfo.split(";")
-    val splitMoveInfoWithoutNumber = splitMoveInfo[0].split("-")
+    val splitMoveInfo = moveInfo.split("-", ";")
 
-    val from = splitMoveInfoWithoutNumber[0]
-    val to = splitMoveInfoWithoutNumber[1]
-    val numberMove = splitMoveInfo[1]
+    val from = splitMoveInfo[0]
+    val to = splitMoveInfo[1]
+    val numberMove = splitMoveInfo[2]
 
     println("From: $from")
     println("To: $to")
