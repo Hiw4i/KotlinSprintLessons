3b1ffdfcd10444defc80af4246c5e963831f35bd
Hiw4i <slava.vaneev8@gmail.com>
2025-11-09 13:11:24 +0300

    KS-5-1 added .random

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_5
 
 fun main() {
-    val number1 = 6
-    val number2 = 7
+    val number1 = (1..10).random()
+    val number2 = (1..10).random()
 
-    println("Сложите 2 числа: $number1 + $number2")
+    println("Сложите 2 числа: $number1 + $number2 ")
     val result = readln().toInt()
 
     if (result == number1 + number2) {
