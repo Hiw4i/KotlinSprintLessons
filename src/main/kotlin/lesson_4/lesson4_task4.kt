8fc9e0f7ed6d3591047168319c4bdf5353e5122d
Hiw4i <slava.vaneev8@gmail.com>
2025-11-09 13:17:25 +0300

    KS-4-4 removed val isLegAndBackToday

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -5,12 +5,11 @@ package org.example.lesson_4
 fun main() {
     val numberOfDayToday = 5
     val isArmsAndAbsToday = numberOfDayToday % 2 != 0
-    val isLegAndBackToday = !isArmsAndAbsToday
 
     println("""
         Упражнения для рук: $isArmsAndAbsToday,
-        Упражнения для ног: $isLegAndBackToday,
-        Упражнения для спины: $isLegAndBackToday,
+        Упражнения для ног: ${!isArmsAndAbsToday},
+        Упражнения для спины: ${!isArmsAndAbsToday},
         Упражнения для пресса: $isArmsAndAbsToday
     """.trimIndent())
 }
