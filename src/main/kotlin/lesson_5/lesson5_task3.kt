0e39b87daee562541caccd0b11c5d44cf3630b7c
Hiw4i <slava.vaneev8@gmail.com>
2025-11-11 17:20:02 +0300

    KS-5-3 added randomness of numbers

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,18 +1,18 @@
 package org.example.lesson_5
 
-
-
 fun main() {
-    val firstCorrectNumber = 13
-    val secondCorrectNumber = 41
+    val firstCorrectNumber = (0..42).random()
+    val secondCorrectNumber = (0..42).random()
 
     println("Введите 2 числа от 0 до 42: ")
     val firstNumber = readln().toInt()
     val secondNumber = readln().toInt()
 
-    if ((firstNumber == firstCorrectNumber && secondNumber == secondCorrectNumber) || (firstNumber == secondCorrectNumber && secondNumber == firstCorrectNumber)) {
+    if ((firstNumber == firstCorrectNumber && secondNumber == secondCorrectNumber) ||
+        (firstNumber == secondCorrectNumber && secondNumber == firstCorrectNumber)) {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if ((firstNumber == firstCorrectNumber || secondNumber == secondCorrectNumber) || (firstNumber == secondCorrectNumber || secondNumber == firstCorrectNumber)) {
+    } else if ((firstNumber == firstCorrectNumber || secondNumber == secondCorrectNumber) ||
+        (firstNumber == secondCorrectNumber || secondNumber == firstCorrectNumber)) {
         println("Вы выиграли утешительный приз!")
     } else {
         println("Неудача!")
