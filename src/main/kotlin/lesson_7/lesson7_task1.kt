23375af9ee3cd17a78b543249ea87928e75e982b
Hiw4i <slava.vaneev8@gmail.com>
2025-12-15 21:20:32 +0300

    KS-7-1 extract ranges from loop

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -3,14 +3,14 @@ package org.example.lesson_7
 fun main() {
     val passwordLength = 6
     var generatedPassword = ""
+    val letter = 'a'..'z'
+    val number = 0..9
 
     for (i in 1..passwordLength) {
         if (i % 2 != 0) {
-            val letter = ('a'..'z').random()
-            generatedPassword += letter
+            generatedPassword += letter.random()
         } else {
-            val number = (0..9).random()
-            generatedPassword += number
+            generatedPassword += number.random()
         }
     }
 
