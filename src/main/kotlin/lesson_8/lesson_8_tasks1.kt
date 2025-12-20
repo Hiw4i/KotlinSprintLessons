ba54d1828cfebdf2f7034b9f4940183a6ea1c6c6
Hiw4i <slava.vaneev8@gmail.com>
2025-12-20 15:45:06 +0300

    KS-8-1 Use sum instead of loop

--- src/main/kotlin/lesson_8/lesson_8_tasks1.kt
+++ src/main/kotlin/lesson_8/lesson_8_tasks1.kt
@@ -3,10 +3,7 @@ package org.example.lesson_8
 fun main() {
     val dailyListOfAds = arrayOf(5, 1, 0, 7, 0, 1, 3)
 
-    var totalOfAds = 0
-    for (adsPerDay in dailyListOfAds) {
-        totalOfAds += adsPerDay
-    }
+    val totalOfAds = dailyListOfAds.sum()
 
     println("Всего просмотрено рекламы за неделю: $totalOfAds")
 }
