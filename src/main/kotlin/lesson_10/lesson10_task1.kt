0cf26e54d8f9e807f6333b8b20c626eb56f0e233
Hiw4i <slava.vaneev8@gmail.com>
2026-01-13 18:03:35 +0300

    KS-10-1 dice game human vs bot

---
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_10
+
+fun main() {
+    val humanResult = numberGenerator()
+    println("Human rolled the dice and $humanResult fell out.")
+
+    val botResult = numberGenerator()
+    println("Bot rolled the dice and $botResult fell out.")
+
+    val message = when {
+        humanResult > botResult -> "Humanity has won."
+        humanResult < botResult -> "Bot has won."
+        else -> "Friendship has won."
+    }
+
+    println(message)
+}
+
+fun numberGenerator(): Int {
+    val number = 1..6
+    return number.random()
+}
