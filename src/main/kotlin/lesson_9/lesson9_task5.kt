1430c95198c6134a8c3e773fc85f0665c98fc485
Hiw4i <slava.vaneev8@gmail.com>
2026-01-19 20:48:49 +0300

    KS-9-5 from mutableListOf to mutableSetOf

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,25 +2,23 @@ package org.example.lesson_9
 
 fun main() {
     println("Enter the 5 ingredients.")
-    val listOfIngredients = mutableListOf<String>()
+    val listOfIngredients = mutableSetOf<String>()
     var numberOfIngredients = 1
 
     while (numberOfIngredients <= 5) {
         println("Enter $numberOfIngredients ingredient: ")
         val ingredient =  readln().lowercase()
-        if (!listOfIngredients.contains(ingredient)) {
-            listOfIngredients.add(ingredient)
-        }
+        listOfIngredients.add(ingredient)
 
         numberOfIngredients++
     }
 
-    listOfIngredients.sort()
+    val sortedListOfIngredients = listOfIngredients.toList().sorted()
 
     val resultListOfIngredients = mutableListOf<String>()
 
-    for (ingredient in listOfIngredients) {
-        if (listOfIngredients.indexOf(ingredient) == 0) {
+    for (ingredient in sortedListOfIngredients) {
+        if (sortedListOfIngredients.indexOf(ingredient) == 0) {
             resultListOfIngredients.add(
                 ingredient.replaceFirstChar {
                     it.uppercase()
