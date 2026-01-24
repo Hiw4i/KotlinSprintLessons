f226cbf39e6ee6429e7fc4e7442e150a7839d4ba
Hiw4i <slava.vaneev8@gmail.com>
2026-01-25 02:49:39 +0300

    KS-9-5 toList has been removed from variable sortedSetOfIngredients

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,23 +2,23 @@ package org.example.lesson_9
 
 fun main() {
     println("Enter the 5 ingredients.")
-    val listOfIngredients = mutableSetOf<String>()
+    val setOfIngredients = mutableSetOf<String>()
     var numberOfIngredients = 1
 
     while (numberOfIngredients <= 5) {
         println("Enter $numberOfIngredients ingredient: ")
         val ingredient =  readln().lowercase()
-        listOfIngredients.add(ingredient)
+        setOfIngredients.add(ingredient)
 
         numberOfIngredients++
     }
 
-    val sortedListOfIngredients = listOfIngredients.toList().sorted()
+    val sortedSetOfIngredients = setOfIngredients.sorted()
 
     val resultListOfIngredients = mutableListOf<String>()
 
-    for (ingredient in sortedListOfIngredients) {
-        if (sortedListOfIngredients.indexOf(ingredient) == 0) {
+    for (ingredient in sortedSetOfIngredients) {
+        if (sortedSetOfIngredients.indexOf(ingredient) == 0) {
             resultListOfIngredients.add(
                 ingredient.replaceFirstChar {
                     it.uppercase()
