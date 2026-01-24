e44f5fb0a3100c350dd50c880139a626d7a63cf2
Hiw4i <slava.vaneev8@gmail.com>
2026-01-25 02:01:33 +0300

    KS-12-1 weather class with empty constructor

---
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_12
+
+class WeatherInfo {
+    var dayTemp = 15
+    var nightTemp = 10
+    var isPrecipitation = false
+
+    fun  printInfo() {
+        println("Day temperature: $dayTemp °C.")
+        println("Night temperature: $nightTemp °C.")
+        println("Precipitation: $isPrecipitation.")
+    }
+}
+
+fun main() {
+
+    val monday = WeatherInfo()
+    monday.dayTemp = 11
+    monday.nightTemp = 7
+    monday.isPrecipitation = true
+
+    val tuesday = WeatherInfo()
+    tuesday.dayTemp = 18
+    tuesday.nightTemp = 15
+    tuesday.isPrecipitation = false
+
+    monday.printInfo()
+    tuesday.printInfo()
+}
